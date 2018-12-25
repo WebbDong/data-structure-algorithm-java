e30cad4b9e0f42c99e2b1f4ebf8a9989a0fc842c
dongwenbin <dwbdeveloper1991@163.com>
2018-12-26 00:05:45 +0800

    链表

--- src/main/java/com/datastructure/demo/algo/linkedlist/SinglyLinkedList.java
+++ src/main/java/com/datastructure/demo/algo/linkedlist/SinglyLinkedList.java
@@ -47,14 +47,12 @@ public class SinglyLinkedList<T> {
      * @return
      */
     public SinglyNode<T> findByValue(T data) {
-        int i = 0;
         SinglyNode<T> currentNode = head;
         while (currentNode != null) {
             if (currentNode.getData() == data
                     || (data != null && data.equals(currentNode.getData()))) {
                 return currentNode;
             }
-            i++;
             currentNode = currentNode.getNext();
         }
         return null;
@@ -128,19 +126,21 @@ public class SinglyLinkedList<T> {
 
         SinglyNode<T> currentNode = head;
         SinglyNode<T> preNode = null;
-        while (currentNode != null) {
-            if (node.getData().equals(currentNode.getData())) {
-                if (preNode == null) {
-                    newNode.setNext(currentNode);
-                    head = newNode;
-                } else {
-                    newNode.setNext(preNode.getNext());
-                    preNode.setNext(newNode);
-                }
-                break;
-            }
+        while (currentNode != null && !node.getData().equals(currentNode.getData())) {
             preNode = currentNode;
             currentNode = currentNode.getNext();
+        }
+
+        if (currentNode == null) {
+            return;
+        }
+
+        if (preNode == null) {
+            newNode.setNext(currentNode);
+            head = newNode;
+        } else {
+            newNode.setNext(preNode.getNext());
+            preNode.setNext(newNode);
         }
     }
 
@@ -168,6 +168,112 @@ public class SinglyLinkedList<T> {
     }
 
     /**
+     * 删除指定索引的节点
+     * @param index
+     */
+    public void deleteByIndex(int index) {
+        if (head == null) {
+            return;
+        }
+
+        SinglyNode<T> currentNode = head;
+        SinglyNode<T> preNode = null;
+
+        for (int i = 0; currentNode != null && i != index; i++) {
+            preNode = currentNode;
+            currentNode = currentNode.getNext();
+        }
+
+        if (currentNode == null) {
+            return;
+        }
+
+        deleteNode(currentNode, preNode);
+    }
+
+    /**
+     * 根据指定的值删除节点
+     * @param value
+     */
+    public void deleteByValue(T value) {
+        if (head == null) {
+            return;
+        }
+
+        SinglyNode<T> currentNode = head;
+        SinglyNode<T> preNode = null;
+        while (currentNode != null && !currentNode.getData().equals(value)) {
+            preNode = currentNode;
+            currentNode = currentNode.getNext();
+        }
+
+        if (currentNode == null) {
+            return;
+        }
+
+        deleteNode(currentNode, preNode);
+    }
+
+    /**
+     * 删除指定的节点
+     * @param node
+     */
+    public void deleteByNode(SinglyNode<T> node) {
+        if (head == null) {
+            return;
+        }
+
+        deleteByValue(node.getData());
+    }
+
+    /**
+     * 反转链表
+     */
+    public SinglyNode<T> inverse() {
+        // 正在遍历的节点
+        SinglyNode<T> current = head;
+        // 下一个要遍历的节点
+        SinglyNode<T> next;
+        // 新链表头节点的引用(指向新链表头节点的指针)
+        SinglyNode<T> newHead = null;
+        while (current != null) {
+            // 将下一个要遍历的节点暂存起来
+            next = current.getNext();
+
+            /**
+             * 将当前节点放到新链表的头部：
+             *    1>将当前节点指向新链表的头部
+             *    2>更新newHead
+             */
+            current.setNext(newHead);
+            newHead = current;
+            // 向后继续遍历
+            current = next;
+        }
+        head = newHead;
+        return newHead;
+    }
+
+    public SinglyNode<T> inverseLinkList(SinglyNode<T> p){
+        SinglyNode<T> pre = null;
+        SinglyNode<T> r = head;
+        SinglyNode<T> next= null;
+        while(r !=p){
+            next = r.getNext();
+
+            r.setNext(pre);
+            pre = r;
+            r = next;
+        }
+
+        r.setNext(pre);
+        //　返回左半部分的中点之前的那个节点
+        //　从此处开始同步像两边比较
+        return r;
+
+    }
+
+    /**
      * 打印所有元素
      */
     public void printAll() {
@@ -177,27 +283,80 @@ public class SinglyLinkedList<T> {
             currentNode = currentNode.getNext();
         }
         System.out.println();
-        System.out.println("-------------------------------");
+    }
+
+    /**
+     * 删除节点
+     * @param currentNode
+     * @param preNode
+     */
+    private void deleteNode(SinglyNode<T> currentNode, SinglyNode<T> preNode) {
+        if (preNode == null) {
+            head = currentNode.getNext();
+            currentNode.setNext(null);
+        } else {
+            preNode.setNext(currentNode.getNext());
+            currentNode.setNext(null);
+        }
     }
 
     public static void main(String[] args) {
+        System.out.println("-------------- insertToHead -----------------");
         SinglyLinkedList<Integer> list1 = new SinglyLinkedList<>();
         list1.insertToHead(100);
         list1.insertToHead(200);
         list1.insertToHead(300);
         list1.printAll();
 
+        System.out.println("-------------- insertToTail -----------------");
         SinglyLinkedList<Integer> list2 = new SinglyLinkedList<>();
         list2.insertToTail(700);
         list2.insertToTail(800);
         list2.insertToTail(900);
+        list2.insertToTail(2000);
+        list2.insertToTail(4000);
+        list2.insertToTail(6000);
         list2.printAll();
 
+        System.out.println("-------------- find -----------------");
         SinglyNode<Integer> node1 = list2.findByIndex(1);
         System.out.println("list2.findByIndex(1) = " + node1);
 
         SinglyNode<Integer> node2 = list2.findByValue(900);
         System.out.println("list2.findByValue(900) = " + node2);
+
+        System.out.println("-------------- insertAfter -----------------");
+        SinglyLinkedList<Integer> list3 = new SinglyLinkedList<>();
+        list3.insertToHead(2000);
+        list3.insertToHead(3000);
+        list3.insertToHead(4000);
+        SinglyNode<Integer> node3 = list3.findByIndex(0);
+        list3.insertAfter(node3, 7000);
+        list3.printAll();
+
+        System.out.println("-------------- insertBefore -----------------");
+        SinglyNode<Integer> node4 =  list3.findByIndex(0);
+        list3.insertBefore(node4, 50000);
+        list3.printAll();
+
+        System.out.println("-------------- deleteByIndex -----------------");
+        list3.deleteByIndex(1);
+        list3.printAll();
+
+        System.out.println("-------------- deleteByValue -----------------");
+        list3.deleteByValue(2000);
+        list3.printAll();
+
+        System.out.println("-------------- deleteByNode -----------------");
+        SinglyNode<Integer> deleteNode = new SinglyNode<>(50000);
+        list3.deleteByNode(deleteNode);
+        list3.printAll();
+
+        System.out.println("-------------- inverse -----------------");
+        list2.printAll();
+        list2.inverse();
+//        list2.inverseLinkList(list2.head);
+        list2.printAll();
     }
 
 }
