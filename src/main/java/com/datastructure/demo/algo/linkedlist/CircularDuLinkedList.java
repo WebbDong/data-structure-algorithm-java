a643c428f7cc73926eb26550399488c886567838
Webb Dong <webb@swell.link>
2019-01-02 19:07:53 +0800

    双向循环链表

---
+++ src/main/java/com/datastructure/demo/algo/linkedlist/CircularDuLinkedList.java
@@ -0,0 +1,42 @@
+package com.datastructure.demo.algo.linkedlist;
+
+import com.datastructure.demo.algo.linkedlist.model.NormalNode;
+
+/**
+ * @Description: 双向循环链表
+ * @Author: Webb Dong
+ * @CreateDate: 2019/01/02 10:50
+ * @UpdateUser: Webb Dong
+ * @UpdateDate: 2019/01/02 10:50
+ * @UpdateRemark:
+ * @Version: 1.0.0
+ */
+public class CircularDuLinkedList<T> {
+
+    /**
+     * 头节点
+     */
+    private NormalNode<T> head;
+
+    /**
+     * 尾结点
+     */
+    private NormalNode<T> last;
+
+    /**
+     * 插入到头部
+     * @param data
+     */
+    public void insertToHead(T data) {
+
+    }
+
+    /**
+     * 插入到头部
+     * @param newNode
+     */
+    public void insertToHead(NormalNode<T> newNode) {
+
+    }
+
+}
