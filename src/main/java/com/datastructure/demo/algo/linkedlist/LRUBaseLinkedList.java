3969f159166b05ec806dd51d479156e621659ce7
WebbDong <webbdong@swell.link>
2019-02-13 22:14:55 +0800

    基于链表的LRU

--- src/main/java/com/datastructure/demo/algo/linkedlist/LRUBaseLinkedList.java
+++ src/main/java/com/datastructure/demo/algo/linkedlist/LRUBaseLinkedList.java
@@ -3,10 +3,10 @@ package com.datastructure.demo.algo.linkedlist;
 import com.datastructure.demo.algo.linkedlist.model.NormalNode;
 
 /**
- * @Description: 基于链表的最近最少使用策略LRU(Least Recently Used)
+ * @Description: 基于不带头链表的最近最少使用策略LRU(Least Recently Used)
  * 维护一个有序单链表，越靠近链表尾部的结点是越早之前访问的。当有一个新的数据被访问时，我们从链表头开始顺序遍历
  *   1、如果此数据之前已经被缓存在链表中了，我们遍历得到这个数据对应的结点，并将其从原来的位置删除，然后再插入到链表的头部。
- *   2、如果次2数据没有再缓存链表中，又分为2种情况
+ *   2、如果此数据没有再缓存链表中，又分为2种情况
  *      a：如果此时缓存未满，则将此结点直接插入到链表的头部；
  *      b：如果此时缓存已满，则链表尾结点删除，将新的数据结点插入链表的头部；
  * @Author: Webb Dong
@@ -42,6 +42,97 @@ public class LRUBaseLinkedList<T extends Comparable<T>> {
         this.head = null;
         this.capacity = DEFAULT_CAPACITY;
         this.size = 0;
+    }
+
+    /**
+     * 添加元素
+     * @param data
+     */
+    public void add(T data) {
+        if (head == null) {
+            head = new NormalNode<>(data);
+        } else {
+            NormalNode<T> preNode = findPreNode(data);
+            if (preNode != null) {
+                // 元素在链表中
+                deleteNextNode(preNode);
+                insertNodeAtHead(data);
+            } else {
+                // 元素不在链表中
+                if (size >= capacity) {
+                    // 队列大小已满，删除尾节点
+                    deleteEndNode();
+                }
+                insertNodeAtHead(data);
+            }
+        }
+    }
+
+    /**
+     * 删除尾节点
+     */
+    private void deleteEndNode() {
+        NormalNode<T> currentNode = head;
+        NormalNode<T> preNode = head;
+        if (currentNode != null) {
+            while (true) {
+                currentNode = currentNode.getNext();
+                if (currentNode == null) {
+                    break;
+                }
+                preNode = currentNode;
+            }
+
+            preNode.setNext(null);
+            size--;
+        }
+    }
+
+    /**
+     * 插入节点到头部
+     * @param data
+     */
+    private void insertNodeAtHead(T data) {
+        NormalNode<T> newNode = new NormalNode<>(data);
+        newNode.setData(data);
+        newNode.setNext(head.getNext());
+        head = newNode;
+        size++;
+    }
+
+    /**
+     * 删除下一个节点
+     * @param preNode
+     */
+    private void deleteNextNode(NormalNode<T> preNode) {
+        NormalNode<T> temp = preNode.getNext();
+        preNode.setNext(temp.getNext());
+        temp.setNext(null);
+        temp = null;
+        size--;
+    }
+
+    /**
+     * 查找指定数据节点的上一个节点
+     * @param data
+     * @return
+     */
+    private NormalNode<T> findPreNode(T data) {
+        NormalNode<T> currentNode = head;
+        NormalNode<T> preNode = head;
+        if (currentNode != null) {
+            while (true) {
+                currentNode = currentNode.getNext();
+                if (currentNode == null) {
+                    break;
+                }
+                if (data.equals(currentNode.getData())) {
+                    return preNode;
+                }
+                preNode = currentNode;
+            }
+        }
+        return null;
     }
 
 }
