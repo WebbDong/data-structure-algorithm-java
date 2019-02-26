af45d590ad45e20b2dd2c24b0d26f39a947c4a8c
Webb Dong <webb@swell.link>
2019-02-26 17:58:02 +0800

    队列

---
+++ src/main/java/com/datastructure/demo/algo/queue/ArrayDilatationQueue.java
@@ -0,0 +1,24 @@
+package com.datastructure.demo.algo.queue;
+
+/**
+ * @Description: java类作用描述
+ * @Author: Webb Dong
+ * @CreateDate: 2019/02/26 17:44
+ * @UpdateUser: Webb Dong
+ * @UpdateDate: 2019/02/26 17:44
+ * @UpdateRemark:
+ * @Version: 1.0.0
+ */
+public class ArrayDilatationQueue<T> {
+
+    /**
+     * 元素
+     */
+    private T[] elements;
+
+    /**
+     * 栈大小
+     */
+    private int capacity;
+
+}
