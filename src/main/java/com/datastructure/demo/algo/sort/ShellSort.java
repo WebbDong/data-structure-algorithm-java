8efd685142ccc6be08904cbee2e1d0f84e42f296
WebbDong <webbdong@swell.link>
2019-04-13 00:29:22 +0800

    希尔排序

--- src/main/java/com/datastructure/demo/algo/sort/ShellSort.java
+++ src/main/java/com/datastructure/demo/algo/sort/ShellSort.java
@@ -9,12 +9,16 @@ public class ShellSort {
 
     public static void main(String[] args) {
         int[] arr1 = {4, 5, 6, 1, 3, 2};
-        shellSort(arr1);
+        shellKnuthSort(arr1);
         System.out.println(Arrays.toString(arr1));
 
         int[] arr2 = {6, 5, 4, 3, 2, 1};
-        shellSort(arr2);
+        shellKnuthSort(arr2);
         System.out.println(Arrays.toString(arr2));
+
+        int[] arr3 = {1, 2, 3, 4, 5, 6};
+        shellKnuthSort(arr3);
+        System.out.println(Arrays.toString(arr3));
     }
 
     /**
@@ -29,6 +33,51 @@ public class ShellSort {
         // 间距
         int h = len / 2;
         while (h >= 1) {
+            for (int i = h; i < len; i++) {
+                int value = arr[i];
+                int j = i - h;
+                for (; j >= 0; j -= h) {
+                    if (arr[j] > value) {
+                        arr[j + h] = arr[j];
+                    } else {
+                        break;
+                    }
+                }
+                arr[j + h] = value;
+            }
+            h /= 2;
+        }
+    }
+
+    /**
+     * 希尔排序，Knuth增量序列
+     * @param arr
+     */
+    private static void shellKnuthSort(int[] arr) {
+        int len = arr.length;
+        if (len <= 1) {
+            return;
+        }
+        // Knuth增量序列间距
+        int h = 1;
+        while (h <= len / 3) {
+            h = h * 3 + 1;
+        }
+
+        while (h >= 1) {
+            for (int i = h; i < len; i++) {
+                int value = arr[i];
+                int j = i - h;
+                for (; j >= 0; j -= h) {
+                    if (arr[j] > value) {
+                        arr[j + h] = arr[j];
+                    } else {
+                        break;
+                    }
+                }
+                arr[j + h] = value;
+            }
+            h = (h - 1) / 3;
         }
     }
 
