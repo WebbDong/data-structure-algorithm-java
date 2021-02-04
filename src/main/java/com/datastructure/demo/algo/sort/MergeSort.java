38845cdc1af04d405f9a30fc4c59e3ed603f864c
Webb Dong <wangwebb1991@gmail.com>
2021-02-04 22:53:24 +0800

    归并排序

--- src/main/java/com/datastructure/demo/algo/sort/MergeSort.java
+++ src/main/java/com/datastructure/demo/algo/sort/MergeSort.java
@@ -20,46 +20,30 @@ public class MergeSort {
     }
 
     // --------------- 递归方式 -------------------
+
     /**
      * 递归方式归并排序
      * @param arr
      */
     public static void recursionMergeSort(int[] arr) {
-        sort(arr, 0, arr.length - 1);
+        sort(arr, 0, arr.length);
     }
 
-    public static void sort(int[] arr, int l, int r) {
-        if(l == r) {
+    public static void sort(int[] arr, int begin, int end) {
+        // 如果元素只有一个或者没有元素直接返回
+        if (end - begin < 2) {
             return;
         }
+
         // 除以2
-        int mid = l + ((r - l) >> 1);
-        sort(arr, l, mid);
-        sort(arr, mid + 1, r);
-        merge(arr, l, mid, r);
+        int mid = (begin + end) >> 1;
+        sort(arr, begin, mid);
+        sort(arr, mid, end);
+        merge();
     }
 
-    public static void merge(int[] arr, int l, int mid, int r) {
-        int[] temp = new int[r - l + 1];
-        int i = 0;
-        int p1 = l;
-        int p2 = mid + 1;
-        // 比较左右两部分的元素，哪个小，把那个元素填入temp中
-        while (p1 <= mid && p2 <= r) {
-            temp[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
-        }
-        // 上面的循环退出后，把剩余的元素依次填入到temp中
-        // 以下两个while只有一个会执行
-        while (p1 <= mid) {
-            temp[i++] = arr[p1++];
-        }
-        while (p2 <= r) {
-            temp[i++] = arr[p2++];
-        }
-        // 把最终的排序的结果复制给原数组
-        for (i = 0; i < temp.length; i++) {
-            arr[l + i] = temp[i];
-        }
+    private static void merge() {
+
     }
 
 }
