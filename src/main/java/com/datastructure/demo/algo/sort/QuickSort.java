2add724dbd705d93a681caddd379327fef6f5748
Webb Dong <wangwebb1991@gmail.com>
2021-02-21 00:25:25 +0800

    快速排序

--- src/main/java/com/datastructure/demo/algo/sort/QuickSort.java
+++ src/main/java/com/datastructure/demo/algo/sort/QuickSort.java
@@ -7,13 +7,11 @@ public class QuickSort {
 
     public static void main(String[] args) {
         int[] arr = {8, 10, 2, 3, 6, 1, 5};
-        quickSort(arr, 0, arr.length - 1);
+        quickSort(arr, 0, arr.length);
     }
 
-    private static void quickSort(int[] arr, int p, int r) {
-        if (p >= r) {
-            return;
-        }
+    private static void quickSort(int[] arr, int b, int e) {
+
     }
 
 }
